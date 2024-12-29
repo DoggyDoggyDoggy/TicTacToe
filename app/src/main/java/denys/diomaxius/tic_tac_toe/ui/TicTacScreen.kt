eeb8266343bc5af9601f4e9d8ae96d5eb2012fa6
Doggy <pedan.denys@gmail.com>
2024-12-30 00:53:25 +1300

    Updated UI + Reset button

--- app/src/main/java/denys/diomaxius/tic_tac_toe/ui/TicTacScreen.kt
+++ app/src/main/java/denys/diomaxius/tic_tac_toe/ui/TicTacScreen.kt
@@ -1,6 +1,6 @@
 package denys.diomaxius.tic_tac_toe.ui
 
-import android.util.Log
+import android.widget.Space
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -8,86 +8,117 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.State
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.capitalize
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import denys.diomaxius.tic_tac_toe.R
+import java.util.Locale
 
 
 @Composable
-fun TicTacScreen() {
-    TicTacToeBoard()
-}
-
-/*
-Box(
-                modifier = Modifier
-                    .border(BorderStroke(2.dp, Color.Gray))
-                    .size(50.dp)
-                    .clickable { ticTacToeViewModel.placeState(0)}
-            ) {
-                if (boardState.value?.get(0) == "x") {
-                    Icon(
-                        modifier = Modifier.align(alignment = Alignment.Center),
-                        painter = painterResource(id = R.drawable.x),
-                        contentDescription = ""
-                    )
-                } else if (boardState.value?.get(0) == "o") {
-                    Icon(
-                        modifier = Modifier.align(alignment = Alignment.Center),
-                        painter = painterResource(id = R.drawable.o),
-                        contentDescription = ""
-                    )
-                }
-            }
-*/
-@Composable
-fun TicTacToeBoard(
+fun TicTacScreen(
     ticTacToeViewModel: TicTacScreenViewModel = viewModel()
 ) {
     val boardState = ticTacToeViewModel.boardState.observeAsState()
+    val playerTurn = ticTacToeViewModel.playerTurn.observeAsState()
 
     Column(
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween // Пространство между элементами
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.weight(1f)
+        ) {
+            playerTurn.value?.let {
+                Text(
+                    text = "Player: ${it.replaceFirstChar { char -> char.uppercaseChar() }}",
+                    fontSize = 72.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TicTacToeBoard(
+                modifier = Modifier.weight(1f),
+                placeState = { ticTacToeViewModel.placeState(it) },
+                boardState = boardState
+            )
+        }
+
+        Button(
+            onClick = { ticTacToeViewModel.resetBoard() },
+            modifier = Modifier.padding(bottom = 20.dp) // Отступ от нижнего края
+        ) {
+            Text(text = "Start new game")
+        }
+    }
+
+}
+
+@Composable
+fun TicTacToeBoard(
+    placeState: (Int) -> Unit,
+    boardState: State<List<String>?>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         for (i in 0..2) {
             Row(
-
+                modifier = Modifier
+                    .fillMaxWidth()
             ) {
                 for (x in 0..2) {
                     val position = i * 3 + x
                     Box(
                         modifier = Modifier
+                            .weight(1f)
+                            .aspectRatio(1f)
                             .border(BorderStroke(2.dp, Color.Gray))
-                            .size(100.dp)
                             .clickable {
-                                if (boardState.value?.get(position).isNullOrEmpty()
+                                if (boardState.value
+                                        ?.get(position)
+                                        .isNullOrEmpty()
                                 ) {
-                                    ticTacToeViewModel.placeState(position)
+                                    placeState(position)
                                 }
                             },
                         contentAlignment = Alignment.Center
                     ) {
                         when (boardState.value?.get(position)) {
                             "x" -> Icon(
+                                modifier = Modifier.fillMaxSize(),
                                 painter = painterResource(id = R.drawable.x),
                                 contentDescription = null
                             )
 
                             "o" -> Icon(
+                                modifier = Modifier.fillMaxSize(),
                                 painter = painterResource(id = R.drawable.o),
                                 contentDescription = null
                             )
--- app/src/main/java/denys/diomaxius/tic_tac_toe/ui/TicTacScreenViewModel.kt
+++ app/src/main/java/denys/diomaxius/tic_tac_toe/ui/TicTacScreenViewModel.kt
@@ -5,14 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import denys.diomaxius.tic_tac_toe.BoardState
 
-class TicTacScreenViewModel: ViewModel() {
+class TicTacScreenViewModel : ViewModel() {
     private val _boardState = MutableLiveData(BoardState.boardState)
     val boardState: LiveData<List<String>> = _boardState
 
     private val _playerTurn = MutableLiveData("x")
+    val playerTurn: LiveData<String> = _playerTurn
 
-
-    private fun changePlayerTurn() : String {
+    private fun changePlayerTurn(): String {
         return if (_playerTurn.value == "x") {
             _playerTurn.value = "o"
             "x"
@@ -22,11 +22,16 @@ class TicTacScreenViewModel: ViewModel() {
         }
     }
 
-    fun placeState (position: Int) {
-       _boardState.value?.let { currentState->
-           val updatedState = currentState.toMutableList()
-           updatedState[position] = changePlayerTurn()
-           _boardState.value = updatedState
-       }
+    fun resetBoard() {
+        _boardState.value = BoardState.boardState
+        _playerTurn.value = "x"
+    }
+
+    fun placeState(position: Int) {
+        _boardState.value?.let { currentState ->
+            val updatedState = currentState.toMutableList()
+            updatedState[position] = changePlayerTurn()
+            _boardState.value = updatedState
+        }
     }
 }
